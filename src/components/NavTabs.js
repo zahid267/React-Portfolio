995a8024209053d87e8bf58b7d7ab1119bb55ee0
Muhammad Zahid <mrzahid@rogers.com>
2021-07-12 22:01:41 -0400

    Updating all files

---
+++ src/components/NavTabs.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './pages/App.css';
+// Here we are using object destructuring assignment to pluck off our variables from the props object
+// We assign them to their own variable names
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <header className="header">
+      <h1>{`Muhammad`}<span className="seo">{`Zahid`}</span></h1>
+    <ul className="nav nav-tabs">
+      
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          // This is a conditional (ternary) operator that checks to see if the current page is "Resume"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+    </header>
+  );
+}
+
+export default NavTabs;
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import './App.css';
+
+export default function Portfolio() {
+  return (
+    <div className="work">
+      <h1>Portfolio</h1>
+      <div>
+          <span className="proj" style={{backgroundImage:'url("/images/code_refactor.png")'}}>
+                <label className="projlbl" onClick={() => window.open('https://zahid267.github.io/code_refactor/')}>
+                    <strong>Code Refactor</strong><br/>
+                    <small>HTML/CSS</small>
+                </label>
+                <label className="projgit" onClick={() => window.open('https://github.com/zahid267/code_refactor')}>
+                <strong>Code Refactor</strong><br />
+                  <small>Git Repo</small>
+                </label>
+            </span>
+
+
+            <span className="proj" style={{backgroundImage:'url("/images/Sports_Pad_Demo.gif")'}}>
+                <label className="projlbl" onClick={() => window.open('https://zahid267.github.io/SportsPad/')}>
+                    <strong>Sports Pad</strong><br />
+                    <small>Server Side APIs</small>
+                </label>
+                <label className="projgit" onClick={() => window.open('https://github.com/zahid267/SportsPad')}>
+                <strong>Sports Pad</strong><br />
+                  <small>Git Repo</small>
+                </label>
+            </span>
+            
+            <span className="proj" style={{backgroundImage:'url("/images/Note_Taker_Demo.gif")'}}>
+                <label className="projlbl" onClick={() => window.open('https://note-taker-zahid.herokuapp.com/')}>
+                    <strong>Note Taker</strong><br />
+                    <small>Express.js</small>
+                </label>
+                <label className="projgit" onClick={() => window.open('https://github.com/zahid267/Note_Taker')}>
+                <strong>Note Taker</strong><br />
+                  <small>Git Repo</small>
+                </label>
+            </span>
+            <span className="proj" style={{backgroundImage:'url("/images/shop-list.png")'}}>
+                <label className="projlbl" onClick={() => window.open('https://murmuring-headland-20273.herokuapp.com/')}>
+                    <strong title="Project 2 - Marketing Revenue Tracker">Revenue Tracker</strong><br />
+                    <small>Model View Controller</small>
+                </label>
+                <label className="projgit" onClick={() => window.open('https://github.com/zahid267/Revenue-Tracker')}>
+                <strong title="Project 2 - Marketing Revenue Tracker">Revenue Tracker</strong><br />
+                  <small>Git Repo</small>
+                </label>
+            </span>
+             
+            <span className="proj" style={{backgroundImage:'url("/images/Fitness_Tracker.gif")'}}>
+                <label className="projlbl" onClick={() => window.open('https://degrassi-parliament-00065.herokuapp.com/')}>
+                    <strong >Workout Tracker</strong><br />
+                    <small>NoSQL - MongoDB</small>
+                </label>
+                <label className="projgit" onClick={() => window.open('https://github.com/zahid267/workout_tracker')}>
+                <strong>Workout Tracker</strong><br />
+                  <small>Git Repo</small>
+                </label>
+            </span>
+            <span className="proj" style={{backgroundImage:'url("/images/Budget-Tracker-screenshot.png")'}}>
+                <label className="projlbl" onClick={() => window.open('https://fierce-fjord-72791.herokuapp.com/')}>
+                    <strong alt="Online/Offline Budget Trackers">Budget Tracker</strong><br />
+                    <small title="Offline Installation">PWA - NoSQL</small>
+                </label>
+                <label className="projgit" onClick={() => window.open('https://github.com/zahid267/Budget-Tracker')}>
+                <strong title="Online/Offline Budget Trackers">Budget Tracker</strong><br />
+                  <small>Git Repo</small>
+                </label>
+            </span>
+                         
+                     
+      </div>
+      
+
+
+    </div>
+  );
+}
