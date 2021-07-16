8f38dfdf2944c92e4534759c572857f6deffc3bb
Muhammad Zahid <mrzahid@rogers.com>
2021-07-16 19:15:56 -0400

    Updating Resume and README files

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,9 +2,37 @@ import React from 'react';
 
 export default function Resume() {
   return (
-    <div>
-      <h1>Resume</h1>
-      <a href="./assets/ZAHIDRESUME.pdf" download="resumeZahid">Download the pdf</a>
+    <div className="work">
+      <a href="./assets/ZAHIDRESUME.pdf" download="resumeZahid">Download the Resume</a>
+      <div>
+               
+                  <h3>Programmer Analyst</h3>
+                            <h4>Waterloo Information Systems Ltd., Waterloo, ON</h4>
+                            <h5>July 2002 – Present</h5>
+                                <br />
+                            <ul>
+                                <li>Develop web applications utilizing uniVerse database, PICK BASIC, JSP, java, Perl, javascript, jQuary, Ajax, jBoss, Wildfly, Canvas 2d, HTML, HTML5, DHTML, CSS, RedBack, U2 WebDe.</li>
+                                <li>Develop custom software using appropriate software language</li>
+                                <li>Develop new applications using Pick uniVerse and PICK BASIC</li>
+                                <li>Developed web methods to process credit card payment</li>
+                                <li>Developed applications to make LWP Requests to get XML, Json data from affiliated websites</li>
+                                <li>Created applications to retrieve prices, images and product availability from the affiliated sites</li>
+                                <li>Developed JSP code generation software using uniVerse database</li>
+                                <li>Maintain and modify existing software to fulfill customer requirements</li>
+                                <li>Develop new Web applications interacting with database</li>
+                                <li>Maintain the existing Web applications</li>
+                                <li>Create new database files and modify the existing ones</li>
+                                <li>Test applications for 100% precision</li>
+                                <li>Debug and trouble-shoot existing software</li>
+                                <li>Experience developing web pages to maintain data in a table format.</li>
+                                <li>Experience with resolving cross bowser issues</li>
+                                <li>Experience with Macromedia and Interdev</li>
+                                <li>Experience with Komodo (Perl editor)</li>
+                                <li>Experience developing Mathematics learning tools for up to grade 8</li>
+                                <li>Interaction with customers to satisfy needs, including email/telephone support</li>
+                                <li>Develop applications to send/receive files to/from others software vendors of our customers to update database</li>
+                            </ul>
+                </div>
     </div>
   );
 }
